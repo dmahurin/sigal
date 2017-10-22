a24b2e27f1caaf84868af685fea1996906dbb0d9
Don Mahurin <dm@openright.org>
2017-10-22 17:02:45 +0000

    use uglifyjs rather for js minify

--- make-min.sh
+++ make-min.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-java -jar _work/compiler.jar --js_output_file ./js/lazy.min.js ./js/lazy.js
-java -jar _work/compiler.jar --js_output_file ./js/sigal.min.js ./js/sigal.js
-php -f build.php
+uglifyjs -c -m < ./js/lazy.js > ./js/lazy.min.js
+uglifyjs -c -m < ./js/sigal.js > ./js/sigal.min.js
+php build.php
